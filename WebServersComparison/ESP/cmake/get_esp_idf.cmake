2ce7ac900f3cc7e10c90f956d0ac70cce35d15f9
Krzysztof Mazur <krzmazur1@gmail.com>
2022-07-31 01:05:17 +0200

    Initial ESP setup

---
+++ WebServersComparison/ESP/cmake/get_esp_idf.cmake
@@ -0,0 +1,32 @@
+# credit for the inspiration for ESP-IDF toolchain setup: @patricia-gallardo see:
+# https://github.com/patricia-gallardo/hello_esp/blob/main/CMakeLists.txt
+function(getEspIdf)
+    message(STATUS "FetchContent_Declare esp-idf")
+    include(FetchContent)
+    FetchContent_Declare(
+        esp-idf
+        URL https://github.com/espressif/esp-idf/releases/download/v4.4.1/esp-idf-v4.4.1.zip
+    )
+
+    message(STATUS "FetchContent_GetProperties esp-idf")
+    FetchContent_GetProperties(esp-idf)
+    if (NOT esp-idf_POPULATED)
+        message(STATUS "FetchContent_Populate esp-idf, this may take a while")
+        FetchContent_Populate(esp-idf)
+        set(ENV{IDF_TOOLS_PATH} ${esp-idf_BINARY_DIR})
+        set(esp-idf_SOURCE_DIR  "${esp-idf_SOURCE_DIR}" CACHE INTERNAL "esp-idf_SOURCE_DIR")
+        file(GLOB IDF_TOOLS_PATH_FILES $ENV{IDF_TOOLS_PATH}/* )
+        if (NOT IDF_TOOLS_PATH_FILES)
+            message(STATUS "Installing in: " $ENV{IDF_TOOLS_PATH})
+            if (WIN32)
+                message(STATUS "Install dependencies for Windows")
+                execute_process(COMMAND "install.bat" $ENV{IDF_TARGET} WORKING_DIRECTORY ${esp-idf_SOURCE_DIR})
+            else ()
+                message(STATUS "Install dependencies for MacOS/Linux")
+                execute_process(COMMAND "./install.sh" $ENV{IDF_TARGET} WORKING_DIRECTORY ${esp-idf_SOURCE_DIR})
+            endif ()
+        endif()
+    endif ()
+    
+
+endfunction(getEspIdf)
